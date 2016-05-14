b20b8e029b04dc1649dfc6dedd9ac8a2b03ff7c0
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-05-14 17:41:47 -0600

    added a monster sprite and some code notes

--- SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
@@ -10,15 +10,50 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    // 1
+    // set up player sprite
     let player = SKSpriteNode(imageNamed: "player")
     
     override func didMoveToView(view: SKView) {
-        // 2
+        // set background color
         backgroundColor = SKColor.whiteColor()
-        // 3
+        // set starting position
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
-        // 4
+        // create sprite in the view
         addChild(player)
+    }
+    // creates a random number
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    // sets parameters of the random number
+    func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addMonster() {
+        
+        // Create sprite
+        let monster = SKSpriteNode(imageNamed: "monster")
+        
+        // Determine where to spawn the monster along the Y axis
+        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        
+        // Position the monster slightly off-screen along the right edge,
+        // and along a random position along the Y axis as calculated above
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        // Add the monster to the scene
+        addChild(monster)
+        
+        // Determine speed of the monster
+        let actualSpeed = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        // Create the actions
+        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualSpeed))
+        // !this is important to not over-load the memory of the decvice!
+        let actionMoveDone = SKAction.removeFromParent()
+        // Apply the actions to the monster
+        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        
     }
 }
