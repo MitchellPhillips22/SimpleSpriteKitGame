0ac00039b8021b0a5f580150bcb40bbbbab8e1dd
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-05-14 17:07:35 -0600

    Created app
    Created app, replaced ViewController code with new code

---
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
@@ -0,0 +1,45 @@
+//
+//  GameScene.swift
+//  SpriteKitSimpleGame
+//
+//  Created by Mitchell Phillips on 4/27/16.
+//  Copyright (c) 2016 MitchellPhillips. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Avenir Next")
+        myLabel.text = "Hello, World!"
+        myLabel.fontSize = 45
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        self.addChild(myLabel)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            let secondAction = SKAction.applyForce()
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GameViewController.swift
+//  SpriteKitSimpleGame
+//
+//  Created by Mitchell Phillips on 4/27/16.
+//  Copyright (c) 2016 MitchellPhillips. All rights reserved.
+//
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
