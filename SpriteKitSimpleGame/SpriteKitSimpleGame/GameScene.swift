ad95bc47d926e663a019f1b7cf0f324043dfa94c
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-05-14 18:29:23 -0600

    Started adding physics to calculate collisions and behaviors

--- SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameScene.swift
@@ -8,6 +8,7 @@
 
 import SpriteKit
 
+//MARK: - Vector Calculation Functions
 func + (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
@@ -40,9 +41,18 @@ extension CGPoint {
     }
 }
 
+//MARK: - Create Physics
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Monster   : UInt32 = 0b1       // 1
+    static let Projectile: UInt32 = 0b10      // 2
+}
+
+
 class GameScene: SKScene {
     
-    // set up player sprite
+    //MARK: - Set up player sprite
     let player = SKSpriteNode(imageNamed: "player")
     
     override func didMoveToView(view: SKView) {
@@ -60,7 +70,7 @@ class GameScene: SKScene {
                 ])
             ))
     }
-    // creates a random number
+    //MARK: - Create a random number
     func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
@@ -69,6 +79,7 @@ class GameScene: SKScene {
         return random() * (max - min) + min
     }
     
+    //MARK: - Create Monster Sprite
     func addMonster() {
         
         // Create sprite
@@ -96,37 +107,38 @@ class GameScene: SKScene {
         
     }
     
+    //MARK: - Projectile Launching Function
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
-        // 1 - Choose one of the touches to work with
+        // Choose one of the touches to work with
         guard let touch = touches.first else {
             return
         }
         let touchLocation = touch.locationInNode(self)
         
-        // 2 - Set up initial location of projectile
+        // Set up initial location of projectile
         let projectile = SKSpriteNode(imageNamed: "projectile")
         projectile.position = player.position
         
-        // 3 - Determine offset of location to projectile
+        // Determine offset of touch location to projectile
         let offset = touchLocation - projectile.position
         
-        // 4 - Bail out if you are shooting down or backwards
+        // Cancel shot if you are shooting down or backwards
         if (offset.x < 0) { return }
         
-        // 5 - OK to add now - you've double checked position
+        // Add projectile after double-checking direction of shot
         addChild(projectile)
         
-        // 6 - Get the direction of where to shoot
+        // Get the direction of where to shoot
         let direction = offset.normalized()
         
-        // 7 - Make it shoot far enough to be guaranteed off screen
-        let shootAmount = direction * 1000
+        // Make it shoot far enough to be guaranteed off screen
+        let shootDistance = direction * 1000
         
-        // 8 - Add the shoot amount to the current position
-        let realDest = shootAmount + projectile.position
+        // Add the shoot amount to the current position
+        let realDest = shootDistance + projectile.position
         
-        // 9 - Create the actions
+        // Create the actions
         let actionMove = SKAction.moveTo(realDest, duration: 2.0)
         let actionMoveDone = SKAction.removeFromParent()
         projectile.runAction(SKAction.sequence([actionMove, actionMoveDone]))
